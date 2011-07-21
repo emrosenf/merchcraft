237637c8952099b762884c21e482c801baa05911
Evan Rosenfeld <evan.m.rosenfeld@gmail.com>
2011-07-20 17:22:32 -0700

    First version, include devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merchcraft::Application.config.secret_token = '23a051d0e176f38b08a5da4dbc595b4ce246155587a6389cb8144d2158e9106fc32fc01578cb6e6275ea17219fdd3c65aae798c70a4f6baa75bf908648c37a41'
