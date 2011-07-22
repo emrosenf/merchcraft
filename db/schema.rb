e6906d048e5615da5fc805b974082561505614bf
Evan Rosenfeld <evan.m.rosenfeld@gmail.com>
2011-07-22 15:17:15 -0700

    make confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110722064757) do
+ActiveRecord::Schema.define(:version => 20110722220732) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(:version => 20110722064757) do
     t.string   "last_name"
     t.string   "avatar"
     t.string   "uid"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
